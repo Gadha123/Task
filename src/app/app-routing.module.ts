a568769c8b75c9c63a89392ff199fe1b9ef632cc
Gadha Renji <gadharenji3@gmail.com>
2019-03-08 12:38:30 +0530

    Routing example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Demo1Component } from './demo1/demo1.component';
+import { Demo2Component } from './demo2/demo2.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'Demo1',
+    component: Demo1Component
+  },
+  {
+    path: 'Demo2',
+    component: Demo2Component
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pagenotfound', 
+    component: PagenotfoundComponent
+},
+{
+    path: '**', 
+    redirectTo: '/pagenotfound'
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
